166cb45ef795f327e0a00ccc852c42ca21d58b94
sinyoung <sinyoung.kim@ntoday.kr>
2023-05-09 03:22:04 +0900

    초기설정

---
+++ src/main/java/com/sy/personal_project/Personal1Application.java
@@ -0,0 +1,13 @@
+package com.sy.personal_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Personal_projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Personal_projectApplication.class, args);
+	}
+
+}
